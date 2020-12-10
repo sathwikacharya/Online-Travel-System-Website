a0b33f7746434d68dff808362a8df7394c075748
sathwikacharya <60059843+sathwikacharya@users.noreply.github.com>
2020-12-10 12:19:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 var firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB8Ezyr090C6r_l6yJY95haIrEAXoeqVwI",
-    authDomain: "travel-b6374.firebaseapp.com",
-    databaseURL: "https://travel-b6374.firebaseio.com",
-    projectId: "travel-b6374",
-    storageBucket: "travel-b6374.appspot.com",
-    messagingSenderId: "49745142676",
-    appId: "1:49745142676:web:3104985dbf3697c3a82581",
-    measurementId: "G-BSP6G7K6WG"
+   // enter credentials here from the firebase website 
   // Initialize Firebase
 });
 
