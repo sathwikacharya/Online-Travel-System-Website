27eea9018c6c85c4fb3fc926f8d3702e4f8b58ef
sathwikacharya <60059843+sathwikacharya@users.noreply.github.com>
2020-12-10 12:20:57 +0530

    Update chat.js

--- src/chat.js
+++ src/chat.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
 export default class Bot extends React.Component{
-    componentDidMount(){
-        (function (d, m) {
-    var kommunicateSettings =
-      { "appId": "37beb24dfd2311ce533ced59f722532e3", "popupWidget": true, "automaticChatOpenOnNavigation": true };
-    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-    window.kommunicate = m; m._globals = kommunicateSettings;
-  })(document, window.kommunicate || {});
+    //enter credentials here from kommunicate website
     }
     render(){
         return(
